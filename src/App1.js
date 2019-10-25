ea1eacdc78db50b7d7bc5e897205619f99a016f9
pallenJ <pallennellap17@gmail.com>
2019-10-25 16:28:55 +0900

    button 컴포넌트 생성 성공. 단, 리턴에 여러 버튼들을 넣는것은 실패.

--- src/App1.js
+++ src/App1.js
@@ -3,14 +3,18 @@ import React, { Component } from 'react'
 export default class App1 extends Component {
     render() {
         let elt = [];
-        for (let i = 0; i < 10; i++) {
-            elt.push(<h1>Test</h1>);
+        for (let i = 0; i < 5; i++) {
+            elt.push(
+            <h1>Test</h1>
+                );
         }
-        return (
-            <div>
-                {elt}
-            </div>
-        )
+        let aaa = this.buttonTest; 
+        return this.buttonTest("primary");
+    }
+    buttonTest(btnType) {
+        return(
+            <button class={"btn btn-"+btnType} type="submit">Button </button>
+        );
     }
 }
 
