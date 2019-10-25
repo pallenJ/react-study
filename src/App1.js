a030037aa82130e01c1753493027134ae5dcd78c
pallenJ <pallennellap17@gmail.com>
2019-10-25 16:59:03 +0900

    버튼 출력 테스트 잘 됨. 단, 최종적으로 하나의 div에 전체가 감싸져야함. 주의.

--- src/App1.js
+++ src/App1.js
@@ -7,14 +7,40 @@ export default class App1 extends Component {
             elt.push(
             <h1>Test</h1>
                 );
-        }
-        let aaa = this.buttonTest; 
-        return this.buttonTest("primary");
+        } 
+        return this.buttonTest();
     }
-    buttonTest(btnType) {
+    buttonEXE(btnClass, btnName) {
         return(
-            <button class={"btn btn-"+btnType} type="submit">Button </button>
+            <button class={"btn "+btnClass} type="submit">{btnName} </button>
         );
+    }
+    buttonTest(){
+        return (
+            <div id= "btnSample">
+                <div id="btn-larges">
+                 <h3>Btn-larges</h3>   
+                <div>
+                    {this.buttonEXE("btn-default btn-lg","default")}
+                    {this.buttonEXE("btn-primary btn-lg","primary")}
+                    {this.buttonEXE("btn-info btn-lg","info")}
+                    {this.buttonEXE("btn-warning btn-lg","warning")}
+                    {this.buttonEXE("btn-danger btn-lg","danger")}
+                    {this.buttonEXE("btn-secondary btn-lg","secondary")}
+                </div>
+
+                <div>
+                    {this.buttonEXE("btn-outline-primary btn-lg","primary")}
+                    {this.buttonEXE("btn-outline-info btn-lg","info")}
+                    {this.buttonEXE("btn-outline-warning btn-lg","warning")}
+                    {this.buttonEXE("btn-outline-danger btn-lg","danger")}
+                    {this.buttonEXE("btn-outline-secondary btn-lg","secondary")}
+                </div>
+                
+                </div>
+
+            </div>
+            );
     }
 }
 
