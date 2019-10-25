87adf90a3045ff4d9856d52349429e74db41dae8
pallenJ <pallennellap17@gmail.com>
2019-10-25 17:33:34 +0900

    테이블 예제 연습

--- src/App1.js
+++ src/App1.js
@@ -8,13 +8,68 @@ export default class App1 extends Component {
             <h1>Test</h1>
                 );
         } 
-        return this.buttonTest();
+        return (
+            <div class="container pull-center" align="center">
+                <div class="panel col-8" style={{marginTop: 50}}>
+                    <div class="panel-heading btn-primary">
+                <p class="h2">Table 연습예제</p>
+
+                    </div>
+                    <div class="panel-body" style={{marginTop: 30}}>
+                    {this.tableTest()}
+                    </div>
+                </div>
+            </div>
+        );
     }
+
+    tableTest(){
+        return(
+            <table class="table table-bordered table-hover">
+                <tbody>
+               
+
+                <tr>
+                <td>data1</td>
+                <td>data2</td>
+                <td>data3</td>
+                <td>data4</td>
+                </tr>   
+
+                <tr>
+                <td>data5</td>
+                <td>data6</td>
+                <td>data7</td>
+                <td>data8</td>
+                </tr>   
+
+                <tr>
+                <td>data9</td>
+                <td>data10</td>
+                <td>data11</td>
+                <td>data12</td>
+                </tr>   
+                   
+
+                </tbody>
+                <thead class="table-primary">
+                <tr>
+                <th>col1</th>
+                <th>col2</th>
+                <th>col3</th>
+                <th>col4</th>
+                </tr>
+                </thead>
+            </table>
+        );
+    }
+
     buttonEXE(btnClass, btnName) {
         return(
             <button class={"btn "+btnClass} type="submit">{btnName} </button>
         );
     }
+
     buttonTest(){
         return (
             <div id= "btnSample">
@@ -23,6 +78,7 @@ export default class App1 extends Component {
                 <div>
                     {this.buttonEXE("btn-default btn-lg","default")}
                     {this.buttonEXE("btn-primary btn-lg","primary")}
+                    {this.buttonEXE("btn-success btn-lg","success")}
                     {this.buttonEXE("btn-info btn-lg","info")}
                     {this.buttonEXE("btn-warning btn-lg","warning")}
                     {this.buttonEXE("btn-danger btn-lg","danger")}
@@ -30,7 +86,9 @@ export default class App1 extends Component {
                 </div>
 
                 <div>
+                    {this.buttonEXE("btn-outline-default btn-lg text-white","default")}
                     {this.buttonEXE("btn-outline-primary btn-lg","primary")}
+                    {this.buttonEXE("btn-outline-success btn-lg","success")}
                     {this.buttonEXE("btn-outline-info btn-lg","info")}
                     {this.buttonEXE("btn-outline-warning btn-lg","warning")}
                     {this.buttonEXE("btn-outline-danger btn-lg","danger")}
