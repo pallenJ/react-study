f53636726ee8aaca9b9ff838be2ca02974773e48
pallenJ <pallennellap17@gmail.com>
2019-10-27 17:02:01 +0900

    카드테마 완성

--- src/App1.js
+++ src/App1.js
@@ -5,7 +5,9 @@ export default class App1 extends Component {
 
     render() {
 
-        return DivFormPrtc.container(DivFormPrtc.panel());
+        return (
+            DivFormPrtc.cardEXE()
+        )
 
     }
 
@@ -60,6 +62,77 @@ export class DivFormPrtc extends Component {
         )
     }
 
+    static card(classes, data) {
+        return (
+            <div class={"card " + classes} style={data.style}>
+                <div class="card-header">
+                    {data.header}
+                </div>
+                <div class="card-body">
+                    <h4 class="card-title">{data.title}</h4>
+                    <p class="card-text">{data.text}</p>
+                </div>
+                {
+                    data.component != null ?
+                        <div class="card-footer text-muted">
+                            {data.component}
+                        </div> : ""
+                }
+
+            </div>
+            // <div class={"card "+classes} style={{width: 200}}>
+            //  <div class = "card-header">
+            //     <h5 class="card-title">{data.title}</h5>
+            // </div>   
+            // <div class="card-body">
+            // <p class="card-text">{data.text}</p>
+            // {data.component}
+            // </div>
+            // <div class = ></div>
+            // </div>
+
+        )
+    }
+    static cardEXE() {
+        const cardTheme = [
+            { bg: "default", white: false },
+            { bg: "primary", white: true },
+            { bg: "success", white: true },
+            { bg: "warning", white: false },
+            { bg: "danger", white: true },
+            { bg: "info", white: true },
+            { bg: "light", white: false },
+            { bg: "dark", white: true },
+            { bg: "secondary", white: true },
+        ];
+
+        let elt = [];
+
+        cardTheme.forEach(e => {
+            elt.push(DivFormPrtc.card("bg-"+e.bg+(e.white?" text-white":""),
+            {
+                title: "Card Title", text: 
+                <span>
+                <p class="h6">this is card</p>
+                <small>{e.bg} theme</small>
+                </span>
+                , component: <a class={"btn border-white btn-"+e.bg} href="#">btn</a>,
+                header: "card-head",
+                style: { width: 200 }
+            }))
+        })
+
+        
+
+        return (
+
+            <div id="card-collection" class = "form-inline">
+                {
+                    elt
+                }
+            </div>
+        )
+    }
 
 }
 
@@ -107,48 +180,48 @@ export class TablePrtc extends Component {
 
     static tabledata = {
         headers: ["no", "name", "age", "ability"],
-        datas:[
-        {
-            no: "01",
-            name : "JOnathan JOestar",
-            age  :<p><span class = "h5" style = {{color:"#5882FA"}}> 20 </span>d</p>,
-            ability:"hamon"
-        },
-        {
-            no: "02",
-            name : "JOseph JOestar",
-            age  : <p>
-                <span class = "h5" style = {{color:"#00FF00"}}> 18 </span> -> 
-                <span style = {{color:"#A901DB"}}>62</span> -> 
-                <span style = {{color:"##A4A4A4"}}>79</span></p>,
-            ability:"hamon -> hamon+stand(hermit purple)"
-        },
-        {
-            no: "03",
-            name : "JOtaro kuJO",
-            age  : "17 -> 28 -> 30 -> 41",
-            ability:"Star Platina"
-        },
-        {
-            no: "04",
-            name : "JOsuke higashikata",
-            age  : 16,
-            ability:"Crazy Diamond"
-        },
-        {
-            no: "05",
-            name : "GIOrno GIOvanna",
-            age  : 15,
-            ability:"Gold Experience"
-        },
-        {
-            no: "06",
-            name : "JOlyne kuJO",
-            age  : 18,
-            ability:"Stone Free"
-        },
-        
-    ]
+        datas: [
+            {
+                no: "01",
+                name: "JOnathan JOestar",
+                age: <p><span class="h5" style={{ color: "#5882FA" }}> 20 </span>d</p>,
+                ability: "hamon"
+            },
+            {
+                no: "02",
+                name: "JOseph JOestar",
+                age: <p>
+                    <span class="h5" style={{ color: "#00FF00" }}> 18 </span> ->
+                <span style={{ color: "#A901DB" }}>62</span> ->
+                <span style={{ color: "##A4A4A4" }}>79</span></p>,
+                ability: "hamon -> hamon+stand(hermit purple)"
+            },
+            {
+                no: "03",
+                name: "JOtaro kuJO",
+                age: "17 -> 28 -> 30 -> 41",
+                ability: "Star Platina"
+            },
+            {
+                no: "04",
+                name: "JOsuke higashikata",
+                age: 16,
+                ability: "Crazy Diamond"
+            },
+            {
+                no: "05",
+                name: "GIOrno GIOvanna",
+                age: 15,
+                ability: "Gold Experience"
+            },
+            {
+                no: "06",
+                name: "JOlyne kuJO",
+                age: 18,
+                ability: "Stone Free"
+            },
+
+        ]
     };
     static tableSetting() {
         let headerTemp = this.tabledata.headers;
@@ -157,12 +230,12 @@ export class TablePrtc extends Component {
         this.tabledata.datas = dataTemp.map(data =>
             <tr>
                 <td>{data.no}</td>
-                <td class = "h5">{data.name}</td>
+                <td class="h5">{data.name}</td>
                 <td>{data.age}</td>
                 <td>{data.ability}</td>
             </tr>
-             );
-             
+        );
+
     }
 
     static tableTest() {
@@ -170,7 +243,7 @@ export class TablePrtc extends Component {
         return (
             <table class="table table-bordered table-hover">
                 <tbody>
-                {this.tabledata.datas}
+                    {this.tabledata.datas}
                 </tbody>
                 <thead class="table-primary">
                     <tr>
