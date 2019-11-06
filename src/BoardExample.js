4fe9708131c2b1f89b6c6ecbb82b127cff91cbdb
pallenJ <pallennellap17@gmail.com>
2019-11-06 16:50:57 +0900

    글 삭체까지 완료

--- src/BoardExample.js
+++ src/BoardExample.js
@@ -10,12 +10,22 @@ export default class BoardExample extends Component {
     state = {
         boards:
             [{ brdno: 1, brdwriter: 'GIOGIO', brdtitle: '최고다 연수유라쨩!', brddate: new Date() },
-            { brdno: 2, brdwriter: 'JOJO', brdtitle: '연수다 최고유라쨩!!', brddate: new Date() }]
+            { brdno: 2, brdwriter: 'JOJO', brdtitle: '연수다 최고유라쨩!!', brddate: new Date() }],
+        maxNo: 3
     }
-    
-    handleSaveData = (data) => { 
-        this.setState({ boards: this.state.boards.concat({ 
-            brdno: this.state.maxNo++, brddate: new Date(), ...data }) }); }
+
+    handleSaveData = (data) => {
+        this.setState({
+            boards: this.state.boards.concat({
+                brdno: this.state.maxNo++, brddate: new Date(), ...data
+            })
+        });
+    }
+
+    handleRemove = (brdno) => {
+        this.setState({ boards: this.state.boards.filter(row => row.brdno !== brdno) })
+    }
+
 
     render() {
         const { boards } = this.state;
@@ -24,10 +34,10 @@ export default class BoardExample extends Component {
             <Fragment>
 
                 <div class="container" align="center">
-                    
-                    <BoardForm onSaveData={this.handleSaveData}/>
+
+                    <BoardForm onSaveData={this.handleSaveData} />
                     <pre />
-                    <table class="table table-bordered" style = {{width:600}}>
+                    <table class="table table-bordered" style={{ width: 600 }}>
                         <thead class="thead table-primary">
                             <tr>
                                 <th>NO</th>
@@ -37,7 +47,10 @@ export default class BoardExample extends Component {
                             </tr>
                         </thead>
                         <tbody>
-                        { boards.map(row => (<BoardItem key={row.brdno} row={row} />) ) }
+                            {
+                            boards.map(row => (
+                            <BoardItem key={row.brdno} row={row} onRemove={this.handleRemove} onSelectRow={this.handleSelectRow} />))
+                            }
 
                         </tbody>
                     </table>
@@ -49,42 +62,50 @@ export default class BoardExample extends Component {
     }
 }
 
-class BoardItem extends Component { 
+class BoardItem extends Component {
+    handleRemove = () => { 
+        const { row, onRemove } = this.props; 
+        onRemove(row.brdno); 
+    }
+
     render() {
         return (
-        <tr> 
-            <td>{this.props.row.brdno}</td> 
-            <td>{this.props.row.brdtitle}</td> 
-            <td>{this.props.row.brdwriter}</td> 
-            <td>{this.props.row.brddate.toLocaleDateString('ko-KR')}</td> 
-        </tr>);
+            <tr>
+                <td>{this.props.row.brdno}</td> 
+                <td><a onClick={this.handleSelectRow}>{this.props.row.brdtitle}</a></td> 
+                <td>{this.props.row.brdwriter}</td> 
+                <td>{this.props.row.brddate.toLocaleDateString('ko-KR')}</td> 
+                <td><button class="btn btn-danger" onClick={this.handleRemove}> <i class="fa fa-trash" aria-hidden="true"></i> </button></td>
+
+            </tr>);
     }
 }
 
-class BoardForm extends Component { 
-    state = {} 
-    handleChange = (e) => { this.setState({ [e.target.name]: e.target.value }) } 
-    handleSubmit = (e) => { 
-        e.preventDefault(); 
-        this.props.onSaveData(this.state); this.setState({}); } 
-    
-    render() { 
-        return ( 
-            <form onSubmit={this.handleSubmit} style={{width:400}}>
-              
-              
-            <div align="left" style = {{padding:10}} class="border">
-             <div class="form-group">
-                  <label for="title">Title</label>
-                  <input placeholder="title" name="brdtitle" id = "title" onChange={this.handleChange} type="text" class="form-control"/>
-                </div>
-                <div class="form-group">
-                  <label for="name">Name</label>
-                  <input placeholder="name" name="brdwriter" id = "name" onChange={this.handleChange} type="text" class="form-control"/>
-                </div>
-             <button class = "btn btn-success btn-block" type="submit">Save</button> 
-            </div>   
-             </form> 
-             ); 
-             } 
+class BoardForm extends Component {
+    state = {}
+    handleChange = (e) => { this.setState({ [e.target.name]: e.target.value }) }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.onSaveData(this.state); this.setState({});
     }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit} style={{ width: 400 }}>
+
+
+                <div align="left" style={{ padding: 10 }} class="border">
+                    <div class="form-group">
+                        <label for="title">Title</label>
+                        <input placeholder="title" name="brdtitle" id="title" onChange={this.handleChange} type="text" class="form-control" />
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input placeholder="name" name="brdwriter" id="name" onChange={this.handleChange} type="text" class="form-control" />
+                    </div>
+                    <button class="btn btn-success btn-block" type="submit">Save</button>
+                </div>
+            </form>
+        );
+    }
+}
